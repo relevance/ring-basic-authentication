8dbeebd7f23dba7fb80984a112affafcc59a63e2
Remco van 't Veer <remco@remworks.net>
2012-04-02 10:16:44 +0200

    updated api documentation

--- index.clj
+++ index.clj
@@ -1,8 +1,8 @@
 {:namespaces
  ({:source-url
-   "http://github.com/remvee/ring-basic-authentication/blob/837fb7039a372945d03fbaf1c2a4b39ec078540d/src/remvee/ring/middleware/basic_authentication.clj",
-   :wiki-url "remvee.ring.middleware.basic-authentication-api.html",
-   :name "remvee.ring.middleware.basic-authentication",
+   "http://github.com/remvee/ring-basic-authentication/blob/9bf8ccb34902b9aedc6ff28f413c4a3a20c12c22/src/ring/middleware/basic_authentication.clj",
+   :wiki-url "ring.middleware.basic-authentication-api.html",
+   :name "ring.middleware.basic-authentication",
    :author "Remco van 't Veer",
    :doc "HTTP basis authentication middleware for ring."}),
  :vars
@@ -11,16 +11,15 @@
     [app authenticate realm]
     [app authenticate realm denied-response]),
    :name "wrap-basic-authentication",
-   :namespace "remvee.ring.middleware.basic-authentication",
+   :namespace "ring.middleware.basic-authentication",
    :source-url
-   "http://github.com/remvee/ring-basic-authentication/blob/837fb7039a372945d03fbaf1c2a4b39ec078540d/src/remvee/ring/middleware/basic_authentication.clj#L15",
+   "http://github.com/remvee/ring-basic-authentication/blob/9bf8ccb34902b9aedc6ff28f413c4a3a20c12c22/src/ring/middleware/basic_authentication.clj#L30",
    :raw-source-url
-   "http://github.com/remvee/ring-basic-authentication/raw/837fb7039a372945d03fbaf1c2a4b39ec078540d/src/remvee/ring/middleware/basic_authentication.clj",
+   "http://github.com/remvee/ring-basic-authentication/raw/9bf8ccb34902b9aedc6ff28f413c4a3a20c12c22/src/ring/middleware/basic_authentication.clj",
    :wiki-url
-   "/remvee.ring.middleware.basic-authentication-api.html#remvee.ring.middleware.basic-authentication/wrap-basic-authentication",
+   "/ring.middleware.basic-authentication-api.html#ring.middleware.basic-authentication/wrap-basic-authentication",
    :doc
    "Wrap response with a basic authentication challenge as described in\nRFC2617 section 2.\n\nThe authenticate function is called with two parameters, the userid\nand password, and should return a value when the login is valid.  This\nvalue is added to the request structure with the :basic-authentication\nkey.\n\nThe realm is a descriptive string visible to the visitor.  It,\ntogether with the canonical root URL, defines the protected resource\non the server.\n\nThe denied-response is a ring response structure which will be\nreturned when authorization fails.  The appropriate status and\nauthentication headers will be merged into it.  It defaults to plain\ntext 'access denied' response.",
    :var-type "function",
-   :line 15,
-   :file
-   "/home/remco/Development/clojure/ring-basic-authentication/src/remvee/ring/middleware/basic_authentication.clj"})}
+   :line 30,
+   :file "src/ring/middleware/basic_authentication.clj"})}
